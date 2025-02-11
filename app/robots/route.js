ee2a81d3d46b7d900ceb0ef5e83ff855f470559a
MutableTuple <mutabletuple39@gmail.com>
2025-02-11 13:13:00 +0530

    robots & sitemap

--- app/robots/route.js
+++ app/robots/route.js
@@ -1,7 +1,10 @@
 export async function GET() {
   const content = `User-agent: *
   Allow: /
-  
+  Disallow: /profile
+  Disallow: /account
+  Disallow: /login
+  Disallow: /register
   Sitemap: https://schultetable.com/sitemap.xml`;
 
   return new Response(content, {
--- app/sitemap/route.js
+++ app/sitemap/route.js
@@ -1,22 +1,30 @@
 export async function GET() {
   const baseUrl = "https://schultetable.com";
 
-  // Add your pages here (or fetch from your database if dynamic)
-  const pages = ["", "/about", "/blog", "/contact", "/schulte-table-game"];
+  // Public pages that should be indexed
+  const pages = [
+    "",
+    "/about",
+    "/blog",
+    "/contact",
+    "/schulte-table-game",
+    "/login",
+    "/register",
+  ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map(
-        (page) => `
-      <url>
-        <loc>${baseUrl}${page}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-      </url>`
-      )
-      .join("")}
-  </urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map(
+          (page) => `
+        <url>
+          <loc>${baseUrl}${page}</loc>
+          <changefreq>weekly</changefreq>
+          <priority>0.8</priority>
+        </url>`
+        )
+        .join("")}
+    </urlset>`;
 
   return new Response(sitemap, {
     headers: {
