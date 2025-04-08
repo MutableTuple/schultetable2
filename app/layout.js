ee2310a5bc75e5f9c14af1b4e877474dbd16b1b3
MutableTuple <mutabletuple39@gmail.com>
2025-04-08 10:34:06 +0530

    seo done for new pages

--- app/layout.js
+++ app/layout.js
@@ -121,12 +121,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <GoogleAnalytics gaId="G-66EJ7VMS98" />
+        <GoogleAnalytics gaId="G-66EJ7VMS98" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5879583930762494"
           crossOrigin="anonymous"
-        /> */}
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
